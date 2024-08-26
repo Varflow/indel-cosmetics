2682d3c1d15aaacddd5b27a4b2f796d19f4d6f20
Dima Kapish <dima.development@fakel.com.ua>
2024-08-26 23:47:48 +0300

    fix fax

--- components/app/AppFooter.vue
+++ components/app/AppFooter.vue
@@ -1,141 +1,145 @@
 <template>
-  <footer class="footer container">
-    <div class="row gy-5">
-      <div class="col-lg-4">
-        <h4 class="footer-col__title">Контакти</h4>
-        <div class="footer-col__list">
-          <div class="footer-col__item">ТОВ "Індел"</div>
-          <div class="footer-col__item">Вул. М. Грушевського , 39Д</div>
-          <div class="footer-col__item">Одеса, 65031, Україна</div>
-          <a href="tel:+380 50 673-27-09" class="footer-col__item"
-            >Tel./fax: +380 50 673-27-09</a
-          >
-          <a href="tel:+380 95 285-88-27" class="footer-col__item"
-            >Tel./fax: +380 95 285-88-27</a
-          >
-          <a href="tel:+380 48 737-33-25" class="footer-col__item"
-            >Tel./fax: +380 48 737-33-25</a
-          >
-          <a href="mailto:info@indel.com.ua" class="footer-col__item"
-            >info@indel.com.ua</a
-          >
-          <a
-            href="http://indel.com.ua/"
-            target="_blank"
-            class="footer-col__item"
-            >www.indel.com.ua</a
-          >
-        </div>
-      </div>
-      <div class="col-lg-4">
-        <h4 class="footer-col__title">Інгредієнти</h4>
-        <div class="footer-col__list">
-          <NuxtLink
-            v-for="category of ingredientsForView"
-            :to="
-              Boolean(category.children?.length)
-                ? `/ingredients/${category.id}`
-                : `/category/${category.id}`
-            "
-            class="footer-col__item"
-          >
-            {{ category.name }}
-          </NuxtLink>
-          <NuxtLink
-            v-if="hasMaxIngrediests"
-            to="/ingredients"
-            class="footer-col__item"
-          >
-            Всі інгредієнти
-          </NuxtLink>
-        </div>
-      </div>
+    <footer class="footer container">
+        <div class="row gy-5">
+            <div class="col-lg-4">
+                <h4 class="footer-col__title">Контакти</h4>
+                <div class="footer-col__list">
+                    <div class="footer-col__item">ТОВ "Індел"</div>
+                    <div class="footer-col__item">
+                        Вул. М. Грушевського , 39Д
+                    </div>
+                    <div class="footer-col__item">Одеса, 65031, Україна</div>
+                    <a href="tel:+380 50 673-27-09" class="footer-col__item"
+                        >Tel: +380 50 673-27-09</a
+                    >
+                    <a href="tel:+380 95 285-88-27" class="footer-col__item"
+                        >Tel: +380 95 285-88-27</a
+                    >
+                    <a href="tel:+380 48 737-33-25" class="footer-col__item"
+                        >Tel: +380 48 737-33-25</a
+                    >
+                    <a href="mailto:info@indel.com.ua" class="footer-col__item"
+                        >info@indel.com.ua</a
+                    >
+                    <a
+                        href="http://indel.com.ua/"
+                        target="_blank"
+                        class="footer-col__item"
+                        >www.indel.com.ua</a
+                    >
+                </div>
+            </div>
+            <div class="col-lg-4">
+                <h4 class="footer-col__title">Інгредієнти</h4>
+                <div class="footer-col__list">
+                    <NuxtLink
+                        v-for="category of ingredientsForView"
+                        :to="
+                            Boolean(category.children?.length)
+                                ? `/ingredients/${category.id}`
+                                : `/category/${category.id}`
+                        "
+                        class="footer-col__item"
+                    >
+                        {{ category.name }}
+                    </NuxtLink>
+                    <NuxtLink
+                        v-if="hasMaxIngrediests"
+                        to="/ingredients"
+                        class="footer-col__item"
+                    >
+                        Всі інгредієнти
+                    </NuxtLink>
+                </div>
+            </div>
 
-      <div class="col-lg-4">
-        <h4 class="footer-col__title">Використання</h4>
-        <div class="footer-col__list">
-          <NuxtLink
-            v-for="category of applicationsForView"
-            :to="
-              Boolean(category.children?.length)
-                ? `/applications/${category.id}`
-                : `/category/${category.id}`
-            "
-            class="footer-col__item"
-          >
-            {{ category.name }}
-          </NuxtLink>
-          <NuxtLink
-            v-if="hasMaxApplications"
-            to="/applications"
-            class="footer-col__item"
-          >
-            Всі продукти
-          </NuxtLink>
+            <div class="col-lg-4">
+                <h4 class="footer-col__title">Використання</h4>
+                <div class="footer-col__list">
+                    <NuxtLink
+                        v-for="category of applicationsForView"
+                        :to="
+                            Boolean(category.children?.length)
+                                ? `/applications/${category.id}`
+                                : `/category/${category.id}`
+                        "
+                        class="footer-col__item"
+                    >
+                        {{ category.name }}
+                    </NuxtLink>
+                    <NuxtLink
+                        v-if="hasMaxApplications"
+                        to="/applications"
+                        class="footer-col__item"
+                    >
+                        Всі продукти
+                    </NuxtLink>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
 
-    <div class="footer-copyright">
-      Indel Cosmetics © Copyright 2023 - Усі права захищено
-    </div>
-  </footer>
+        <div class="footer-copyright">
+            Indel Cosmetics © Copyright 2023 - Усі права захищено
+        </div>
+    </footer>
 </template>
 
 <script>
 const toView = (collection) => {
-  if (!collection) {
-    return [];
-  }
+    if (!collection) {
+        return [];
+    }
 
-  return collection.slice(0, 4).map((collection) => {
-    return {
-      id: collection.id,
-      name: collection.attributes.Name,
-      children: !collection.attributes.pod_kategoriyas?.data.length
-        ? null
-        : collection.attributes.pod_kategoriyas?.data.map((subcategory) => {
-            return {
-              id: subcategory.id,
-              name: subcategory.attributes.name,
-            };
-          }),
-    };
-  });
+    return collection.slice(0, 4).map((collection) => {
+        return {
+            id: collection.id,
+            name: collection.attributes.Name,
+            children: !collection.attributes.pod_kategoriyas?.data.length
+                ? null
+                : collection.attributes.pod_kategoriyas?.data.map(
+                      (subcategory) => {
+                          return {
+                              id: subcategory.id,
+                              name: subcategory.attributes.name,
+                          };
+                      },
+                  ),
+        };
+    });
 };
 
 export default {
-  async setup() {
-    try {
-      const { find } = useStrapi();
-      const categories = await find("categories", {
-        populate: {
-          pod_kategoriyas: "*",
-        },
-      });
+    async setup() {
+        try {
+            const { find } = useStrapi();
+            const categories = await find("categories", {
+                populate: {
+                    pod_kategoriyas: "*",
+                },
+            });
 
-      const ingredients = categories.data.filter(
-        (category) => category.attributes.section === "ingredients"
-      );
-      const applications = categories.data.filter(
-        (category) => category.attributes.section === "application"
-      );
+            const ingredients = categories.data.filter(
+                (category) => category.attributes.section === "ingredients",
+            );
+            const applications = categories.data.filter(
+                (category) => category.attributes.section === "application",
+            );
 
-      const ingredientsForView = toView(ingredients);
-      const applicationsForView = toView(applications);
+            const ingredientsForView = toView(ingredients);
+            const applicationsForView = toView(applications);
 
-      const hasMaxIngrediests = ingredients.length >= 5;
-      const hasMaxApplications = applications.length >= 5;
+            const hasMaxIngrediests = ingredients.length >= 5;
+            const hasMaxApplications = applications.length >= 5;
 
-      return {
-        ingredientsForView,
-        applicationsForView,
-        hasMaxApplications,
-        hasMaxIngrediests,
-      };
-    } catch (error) {
-      console.log(error);
-    }
-  },
+            return {
+                ingredientsForView,
+                applicationsForView,
+                hasMaxApplications,
+                hasMaxIngrediests,
+            };
+        } catch (error) {
+            console.log(error);
+        }
+    },
 };
 </script>
--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -1,124 +1,137 @@
 <template>
-  <div class="contact-page">
-    <Head>
-      <Title>Indel Cosmetics Contact Us</Title>
-      <Meta name="og:title" content="Indel Cosmetics Contact Us" />
-    </Head>
-    <div class="container">
-      <AppPageBanner
-        title="Зв'яжіться з нами"
-        subtitle="Якщо у вас виникли запитання або ви хочете дізнатися більше про наші інгредієнти, будь ласка, не соромтеся зв’язатися з нами."
-      />
+    <div class="contact-page">
+        <Head>
+            <Title>Indel Cosmetics Contact Us</Title>
+            <Meta name="og:title" content="Indel Cosmetics Contact Us" />
+        </Head>
+        <div class="container">
+            <AppPageBanner
+                title="Зв'яжіться з нами"
+                subtitle="Якщо у вас виникли запитання або ви хочете дізнатися більше про наші інгредієнти, будь ласка, не соромтеся зв’язатися з нами."
+            />
 
-      <div class="contact-content row gy-4">
-        <div class="col-12 col-lg-6">
-          <h4 class="contact-title">Зв'яжіться з нами</h4>
-          <h5 class="contact-subtitle">Ми з нетерпінням чекаємо від вас!</h5>
+            <div class="contact-content row gy-4">
+                <div class="col-12 col-lg-6">
+                    <h4 class="contact-title">Зв'яжіться з нами</h4>
+                    <h5 class="contact-subtitle">
+                        Ми з нетерпінням чекаємо від вас!
+                    </h5>
 
-          <form class="contact-form" @submit.prevent="submit">
-            <div class="contact-form__row">
-              <label for="name" class="form-field col-12">
-                Ім'я*
-                <input
-                  class="form-field__input"
-                  type="text"
-                  id="name"
-                  required
-                  v-model="formData.name"
-                />
-              </label>
-              <label for="email" class="form-field col-12">
-                Email*
-                <input
-                  class="form-field__input"
-                  type="email"
-                  id="email"
-                  required
-                  v-model="formData.email"
-                />
-              </label>
+                    <form class="contact-form" @submit.prevent="submit">
+                        <div class="contact-form__row">
+                            <label for="name" class="form-field col-12">
+                                Ім'я*
+                                <input
+                                    class="form-field__input"
+                                    type="text"
+                                    id="name"
+                                    required
+                                    v-model="formData.name"
+                                />
+                            </label>
+                            <label for="email" class="form-field col-12">
+                                Email*
+                                <input
+                                    class="form-field__input"
+                                    type="email"
+                                    id="email"
+                                    required
+                                    v-model="formData.email"
+                                />
+                            </label>
+                        </div>
+                        <label for="message" class="form-field">
+                            Ваше повідомлення
+                            <textarea
+                                id="message"
+                                v-model="formData.message"
+                                class="form-field__textarea"
+                            />
+                        </label>
+                        <div class="contact-form__actions">
+                            <AppButton variant="black"
+                                >Відправити повідомлення</AppButton
+                            >
+                        </div>
+                    </form>
+                </div>
+                <div class="col-12 col-lg-6 contact-map">
+                    <div class="contact-map-label">
+                        <p class="contact-address">
+                            <b>ТОВ "Індел"</b> <br />
+                            Вул. М. Грушевського , 39Д <br />
+                            Одеса, 65031, Україна<br />
+                            <a href="tel:+380 50 673-27-09"
+                                >Tel: +380 50 673-27-09</a
+                            >
+                            <a href="tel:+380 95 285-88-27"
+                                >Tel: +380 95 285-88-27</a
+                            >
+                            <a href="tel:+380 48 737-33-25"
+                                >Tel: +380 48 737-33-25</a
+                            >
+                        </p>
+                        <p class="contact-email">
+                            <span class="contact-email__label">EMAIL</span
+                            ><br />
+                            <a
+                                href="mailto:info@indel.com.ua"
+                                class="contact-email__value"
+                                >info@indel.com.ua</a
+                            >
+                        </p>
+                    </div>
+
+                    <div class="contact-map-wrapper">
+                        <iframe
+                            src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6148.763269866758!2d30.68519790380269!3d46.48250575607405!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c63137c54e2d99%3A0x67894dd0c04e6317!2z0KHQtdGA0LLRltGB0L3QuNC5INGG0LXQvdGC0YAg0LzQvtGA0YHRjNC60L7Qs9C-INGC0LAg0YDRltGH0LrQvtCy0L7Qs9C-INGC0YDQsNC90YHQv9C-0YDRgtGD!5e0!3m2!1sru!2sua!4v1716382619597!5m2!1sru!2sua"
+                            width="100%"
+                            height="100%"
+                            style="border: 0"
+                            allowfullscreen=""
+                            loading="lazy"
+                            referrerpolicy="no-referrer-when-downgrade"
+                        ></iframe>
+                    </div>
+                </div>
             </div>
-            <label for="message" class="form-field">
-              Ваше повідомлення
-              <textarea
-                id="message"
-                v-model="formData.message"
-                class="form-field__textarea"
-              />
-            </label>
-            <div class="contact-form__actions">
-              <AppButton variant="black">Відправити повідомлення</AppButton>
-            </div>
-          </form>
         </div>
-        <div class="col-12 col-lg-6 contact-map">
-          <div class="contact-map-label">
-            <p class="contact-address">
-              <b>ТОВ "Індел"</b> <br />
-              Вул. М. Грушевського , 39Д <br />
-              Одеса, 65031, Україна<br />
-              <a href="tel:+380 50 673-27-09">Tel./fax: +380 50 673-27-09</a>
-              <a href="tel:+380 95 285-88-27">Tel./fax: +380 95 285-88-27</a>
-              <a href="tel:+380 48 737-33-25">Tel./fax: +380 48 737-33-25</a>
-            </p>
-            <p class="contact-email">
-              <span class="contact-email__label">EMAIL</span><br />
-              <a href="mailto:info@indel.com.ua" class="contact-email__value"
-                >info@indel.com.ua</a
-              >
-            </p>
-          </div>
-
-          <div class="contact-map-wrapper">
-            <iframe
-              src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d6148.763269866758!2d30.68519790380269!3d46.48250575607405!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40c63137c54e2d99%3A0x67894dd0c04e6317!2z0KHQtdGA0LLRltGB0L3QuNC5INGG0LXQvdGC0YAg0LzQvtGA0YHRjNC60L7Qs9C-INGC0LAg0YDRltGH0LrQvtCy0L7Qs9C-INGC0YDQsNC90YHQv9C-0YDRgtGD!5e0!3m2!1sru!2sua!4v1716382619597!5m2!1sru!2sua"
-              width="100%"
-              height="100%"
-              style="border: 0"
-              allowfullscreen=""
-              loading="lazy"
-              referrerpolicy="no-referrer-when-downgrade"
-            ></iframe>
-          </div>
-        </div>
-      </div>
     </div>
-  </div>
 </template>
 
 <script setup>
 import Micromodal from "micromodal";
 
 const formData = reactive({
-  name: "",
-  email: "",
-  message: "",
+    name: "",
+    email: "",
+    message: "",
 });
 
 const submit = async () => {
-  try {
-    await useFetch("/api/mail", {
-      method: "POST",
-      body: {
-        subject: "Заявка с сайта",
-        html: `
+    try {
+        await useFetch("/api/mail", {
+            method: "POST",
+            body: {
+                subject: "Заявка с сайта",
+                html: `
             <h1>${formData.name} надіслав запит із сайту Indel Cosmetics. Зв'яжіться з ним</h1>
             <p><b>Тема</b>: "Форма зворотнього зв'зку "</p>
             <p><b>Email</b>: ${formData.email}</p>
             <p><b>Повідомлення</b>: ${formData.message}</p>
           `,
-      },
-    });
+            },
+        });
 
-    Micromodal.show("success-modal");
-    setTimeout(() => {
-      Micromodal.close("success-modal");
-    }, 5000);
-  } catch (error) {
-    Micromodal.show("error-modal");
-    setTimeout(() => {
-      Micromodal.close("error-modal");
-    }, 5000);
-  }
+        Micromodal.show("success-modal");
+        setTimeout(() => {
+            Micromodal.close("success-modal");
+        }, 5000);
+    } catch (error) {
+        Micromodal.show("error-modal");
+        setTimeout(() => {
+            Micromodal.close("error-modal");
+        }, 5000);
+    }
 };
 </script>
