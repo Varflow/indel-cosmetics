147c6ecaaceb8c42810e2d9aaa0f21683e3182e9
Dima Kapish <dima.development@fakel.com.ua>
2024-08-26 23:30:48 +0300

    added posts sort by created at. added link to indel from logo

--- components/app/AppHeader.vue
+++ components/app/AppHeader.vue
@@ -1,47 +1,49 @@
 <template>
-  <header class="header container">
-    <div class="row align-items-center">
-      <div class="col-3 d-flex d-lg-none align-items-center">
-        <div class="header-mobile__btn" @click="open">
-          <div class="line"></div>
-          <div class="line"></div>
-          <div class="line"></div>
+    <header class="header container">
+        <div class="row align-items-center">
+            <div class="col-3 d-flex d-lg-none align-items-center">
+                <div class="header-mobile__btn" @click="open">
+                    <div class="line"></div>
+                    <div class="line"></div>
+                    <div class="line"></div>
+                </div>
+            </div>
+            <div class="col-6 col-lg-2">
+                <NuxtLink @click="close" to="https://indel.com.ua">
+                    <img src="/images/logo.svg" alt="" class="header-logo" />
+                </NuxtLink>
+            </div>
+
+            <div class="col-3 d-flex d-lg-none"></div>
+            <div
+                class="d-none d-lg-flex col-lg-8 justify-content-end header-menu"
+            >
+                <AppMenu />
+            </div>
+            <div class="col-lg-2 d-lg-flex d-none">
+                <SearchProducts />
+            </div>
         </div>
-      </div>
-      <div class="col-6 col-lg-2">
-        <NuxtLink @click="close" to="/">
-          <img src="/images/logo.svg" alt="" class="header-logo" />
-        </NuxtLink>
-      </div>
+    </header>
 
-      <div class="col-3 d-flex d-lg-none"></div>
-      <div class="d-none d-lg-flex col-lg-8 justify-content-end header-menu">
-        <AppMenu />
-      </div>
-      <div class="col-lg-2 d-lg-flex d-none">
-        <SearchProducts />
-      </div>
-    </div>
-  </header>
-
-  <AppMobileMenu @close="close" :opened="mobileMenuOpened" />
+    <AppMobileMenu @close="close" :opened="mobileMenuOpened" />
 </template>
 
 <script>
 export default {
-  data() {
-    return {
-      mobileMenuOpened: false,
-    };
-  },
+    data() {
+        return {
+            mobileMenuOpened: false,
+        };
+    },
 
-  methods: {
-    open() {
-      this.mobileMenuOpened = true;
+    methods: {
+        open() {
+            this.mobileMenuOpened = true;
+        },
+        close() {
+            this.mobileMenuOpened = false;
+        },
     },
-    close() {
-      this.mobileMenuOpened = false;
-    },
-  },
 };
 </script>
--- components/post/List.vue
+++ components/post/List.vue
@@ -1,47 +1,50 @@
 <template>
-  <div class="row">
-    <div class="col-lg-4 col-xl-6 gy-5" v-for="post of postsForView">
-      <NuxtLink :to="`/posts/${post.id}`" class="link-without-decoration">
-        <PostCard
-          :title="post.title"
-          :category="post.category"
-          :createdAt="post.createdAt"
-          :image="post.image"
-        />
-      </NuxtLink>
+    <div class="row">
+        <div class="col-lg-4 col-xl-6 gy-5" v-for="post of postsForView">
+            <NuxtLink :to="`/posts/${post.id}`" class="link-without-decoration">
+                <PostCard
+                    :title="post.title"
+                    :category="post.category"
+                    :createdAt="post.createdAt"
+                    :image="post.image"
+                />
+            </NuxtLink>
+        </div>
     </div>
-  </div>
 </template>
 
 <script>
 export default {
-  props: ["limit"],
-  async setup(props) {
-    try {
-      const media = useStrapiMedia();
-      const { find } = useStrapi();
+    props: ["limit"],
+    async setup(props) {
+        try {
+            const media = useStrapiMedia();
+            const { find } = useStrapi();
 
-      const posts = await find("novostis", {
-        populate: "*",
-        pagination: { limit: props.limit },
-      });
+            const posts = await find("novostis", {
+                populate: "*",
+                pagination: { limit: props.limit },
+                sort: "createdAt:desc",
+            });
 
-      const postsForView = posts.data.map((post) => {
-        return {
-          id: post.id,
-          image: `${media}${post.attributes.image.data?.attributes.url}`,
-          title: post.attributes.title,
-          text: post.attributes.text,
-          createdAt: new Date(post.attributes.createdAt).toLocaleDateString(),
-        };
-      });
+            const postsForView = posts.data.map((post) => {
+                return {
+                    id: post.id,
+                    image: `${media}${post.attributes.image.data?.attributes.url}`,
+                    title: post.attributes.title,
+                    text: post.attributes.text,
+                    createdAt: new Date(
+                        post.attributes.createdAt,
+                    ).toLocaleDateString(),
+                };
+            });
 
-      return {
-        postsForView,
-      };
-    } catch (error) {
-      console.log(error);
-    }
-  },
+            return {
+                postsForView,
+            };
+        } catch (error) {
+            console.log(error);
+        }
+    },
 };
 </script>
